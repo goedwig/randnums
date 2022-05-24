aea9c827effe83850ac09978a21080577fcfdf8f
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2022-05-24 17:02:14 +0300

    Implement ordering for generations feed

--- api/feed/views.py
+++ api/feed/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 
 from api.feed.serializers import FeedSerializer
 from randomizer.models import RandomizedNumber
@@ -8,3 +8,5 @@ class FeedListAPIView(generics.ListAPIView):
     queryset = RandomizedNumber.objects.all()
     serializer_class = FeedSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['user', 'value']
